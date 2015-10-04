db4a6d3af9299e4467cec70888154696f2da5e01
Prateek Basavapur Swamy <bs.prateek@gmail.com>
2015-10-04 20:58:43 +0000

    Initial commit

---
+++ automate.py
@@ -0,0 +1,32 @@
+import os
+f_write=open("automate_python.sh",'w')
+rootDir = './Lab1'
+def fix_escape_char(path):
+    correct_path=''
+    for char in path:
+        if ' ' in char:
+            correct_path+='\ '
+        elif '(' in char:
+            correct_path+='\('
+        elif ')' in char:
+            correct_path+='\)'
+        else:
+            correct_path+=char
+    return correct_path
+            
+for dirName, subdirList, fileList in os.walk(rootDir):
+    if 'removed' in dirName:
+        continue
+    else:
+        for filename in fileList:
+            if ".py" in filename:
+                path=dirName+"/"+filename
+                correct_path=fix_escape_char(path)
+                script_run="python3 "+correct_path+"\n"
+                cat_command="cat "+correct_path+"\n"
+                f_write.write(cat_command)
+                f_write.write(script_run)
+                empty_space="for i in `seq 1 20`;do echo;done"
+                f_write.write(empty_space+"\n")
+                
+                
---
+++ automate.sh
@@ -0,0 +1,4 @@
+rm -rf Lab1
+unzip -qq Lab\ 1.zip -d Lab1
+python automate.py
+sh automate_python.sh
